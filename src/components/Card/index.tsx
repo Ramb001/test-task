37232e066b6bfbe5c671f826021876ebcd1dee9f
Artem <artem.rmb@bk.ru>
2023-09-23 18:43:04 +0300

    fix: refactoring

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,14 +3,12 @@ import { objType } from "../../constants";
 import styles from "./Card.module.scss";
 
 function Card({ props }: { props: objType }) {
-  const { userId, id, title, body } = props;
+  const { id, title, body } = props;
   const navigate = useNavigate();
 
   return (
     <div className={styles.card}>
-      <div>
-        {userId}.{id}
-      </div>
+      <div>{id}</div>
       <div className={styles.info}>
         <div className={styles.title}>{title}</div>
         <div className={styles.description}>{body}</div>
@@ -18,7 +16,7 @@ function Card({ props }: { props: objType }) {
       <button
         className={styles.button}
         onClick={() => {
-          navigate(`/${userId}/${id}`);
+          navigate(`/${id}`);
         }}
       >
         Просмотр
